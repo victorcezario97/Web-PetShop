019a223c7e2ab1388aebd5142963a12fa4654561
GabrielBCarvalho <gabrielmattheus.carvalho@gmail.com>
2018-06-04 13:51:01 -0300

    Login de admin

--- script/login.js
+++ script/login.js
@@ -1,28 +1,32 @@
 // Valida o login
-function validateLogin(result){
-  window.alert("validateLogin");
+function validateLogin(result, type){
   if(result !== null){
-    // Seta as variáveis do cliente
-    /*
-    sessionStorage.setItem("clientId", result.id);
-    sessionStorage.setItem("clientName", result.name);
-    sessionStorage.setItem("clientUser", result.user);
-    sessionStorage.setItem("clientAddress", result.address);
-    sessionStorage.setItem("clientPhoto", result.photo);
-    sessionStorage.setItem("clientPhone", result.phone);
-    sessionStorage.setItem("clientEmail", result.email);
-    sessionStorage.setItem("clientPassword", result.password);
-    */
-    clientId = result.id;
-    clientName = result.name;
-    clientUser = result.user;
-    clientAddress = result.address;
-    clientPhoto = result.photo;
-    clientPhone = result.phone;
-    clientEmail = result.email;
-    clientPassword = result.password;
+    // Seta as variáveis do usuário logado
+    if(type === "client"){  // Se é um cliente
+      clientId = result.id;
+      clientName = result.name;
+      clientUser = result.user;
+      clientAddress = result.address;
+      clientPhoto = result.photo;
+      clientPhone = result.phone;
+      clientEmail = result.email;
+      clientPassword = result.password;
+      typeLogged = "client";
+      window.alert("Bem-vindo, " + clientName);
+    }
+    else{                 // Se é um admin
+      adminId = result.id;
+      adminName = result.name;
+      adminUser = result.user;
+      adminPhoto = result.photo;
+      adminEmail = result.email;
+      adminEmail = result.email;
+      adminPassword = result.password;
+      typeLogged = "admin";
+      window.alert("Bem-vindo, " + adminName);
+    }
+
     // Abre home
-    window.alert("Bem-vindo, " + clientName);
     window.location.href = "home.html";
   }
   else  // Se não
@@ -31,40 +35,44 @@ function validateLogin(result){
 
 // Chama validateLogin com result = dados do cliente, se email e senha corretos; result = null, caso contrário.
 function getClient(userInput, passwordInput){
-  let result = null, resultEmail = true, resultUser = true;
+  let result = null, resultClientEmail = true, resultClientUser = true, resultAdminEmail = true, resultAdminUser = true;
   
   let objectStore = db.transaction(["clients"]).objectStore("clients");
   let objectStore2 = db.transaction(["clients"]).objectStore("clients");
+  let objectStore3 = db.transaction(["admins"]).objectStore("admins");
+  let objectStore4 = db.transaction(["admins"]).objectStore("admins");
   
   // Procura por email
   var indexEmail = objectStore.index("email").get(userInput);
   var indexUser = objectStore2.index("user").get(userInput);
-  window.alert("Até indexes");
+  var indexEmailAdmin = objectStore3.index("email").get(userInput);
+  var indexUserAdmin = objectStore4.index("user").get(userInput);
+  //window.alert("Até indexes");
 
   indexEmail.onsuccess = function(event){
     if(indexEmail.result != undefined){   // Se achou o email
       if(indexEmail.result.password === passwordInput){ // Se a senha bate
         result = event.target.result;
-        validateLogin(result);
+        validateLogin(result, "client");
         //return;
       }
       else{   // Se a senha não bate
-        if(resultUser === false){
-          validateLogin(result);
+        if(resultClientUser === false && resultAdminUser === false && resultAdminEmail === false){
+          validateLogin(result, null);
           //return;
         }
         else{
-          resultEmail = false;
+          resultClientEmail = false;
         }
       }
     }
     else{   // Se não achou o email
-      if(resultUser === false){
-        validateLogin(result);
+      if(resultClientUser === false && resultAdminUser === false && resultAdminEmail === false){
+        validateLogin(result, null);
         //return;
       }
       else{
-        resultEmail = false;
+        resultClientEmail = false;
       }
     }
   };
@@ -74,26 +82,84 @@ function getClient(userInput, passwordInput){
     if(indexUser.result != undefined){   // Se o user existe
       if(indexUser.result.password === passwordInput){  // Se a senha bate
         result = event.target.result;
-        validateLogin(result);
+        validateLogin(result, "client");
         //return;
       }
       else{ // Se a senha não bate
-        if(resultEmail === false){
-          validateLogin(result);
+        if(resultClientEmail === false && resultAdminUser === false && resultAdminEmail === false){
+          validateLogin(result, null);
           //return;
         }
         else{
-          resultUser = false;
+          resultClientUser = false;
         }
       }
     }
     else{   // Se o user não existe
-      if(resultEmail === false){
-        validateLogin(result);
+      if(resultClientEmail === false && resultAdminUser === false && resultAdminEmail === false){
+        validateLogin(result, null);
         //return;
       }
       else{
-        resultUser = false;
+        resultClientUser = false;
+      }
+    }
+  }; 
+
+  // Procura por email do admin
+  indexEmailAdmin.onsuccess = function(event){
+    if(indexEmailAdmin.result != undefined){   // Se achou o email
+      if(indexEmailAdmin.result.password === passwordInput){ // Se a senha bate
+        result = event.target.result;
+        validateLogin(result, "admin");
+        //return;
+      }
+      else{   // Se a senha não bate
+        if(resultAdminUser === false && resultClientUser === false && resultClientEmail === false){
+          validateLogin(result, null);
+          //return;
+        }
+        else{
+          resultAdminEmail = false;
+        }
+      }
+    }
+    else{   // Se não achou o email
+      if(resultAdminUser === false && resultClientUser === false && resultClientEmail === false){
+        validateLogin(result, null);
+        //return;
+      }
+      else{
+        resultAdminEmail = false;
+      }
+    }
+  };
+
+  // Procura por user
+  indexUserAdmin.onsuccess = function(event){
+    if(indexUserAdmin.result != undefined){   // Se o user existe
+      if(indexUserAdmin.result.password === passwordInput){  // Se a senha bate
+        result = event.target.result;
+        validateLogin(result, "admin");
+        //return;
+      }
+      else{ // Se a senha não bate
+        if(resultAdminEmail === false && resultClientUser === false && resultClientEmail === false){
+          validateLogin(result, null);
+          //return;
+        }
+        else{
+          resultAdminUser = false;
+        }
+      }
+    }
+    else{   // Se o user não existe
+      if(resultAdminEmail === false && resultClientUser === false && resultClientEmail === false){
+        validateLogin(result, null);
+        //return;
+      }
+      else{
+        resultAdminUser = false;
       }
     }
   }; 
