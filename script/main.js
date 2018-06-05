472d835e6d8ff7a34f2e4f279235834c0af82ddb
Cruwal <wallace.souza@usp.br>
2018-06-05 00:53:18 -0300

    SPA home.html

--- script/main.js
+++ script/main.js
@@ -2,3 +2,14 @@ $(document).ready(function(){
  $('.header').height($(window).height());
 
 })
+
+function singlePage(page) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", page, true);
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			document.getElementById("troca").innerHTML = xhttp.responseText;
+		}
+	}
+	xhttp.send();
+}
