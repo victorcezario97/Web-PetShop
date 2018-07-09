250da3339015543c06f0eb31a950e52c5df3808b
Cruwal <wallace.souza@usp.br>
2018-07-08 23:50:07 -0300

    Server 100%

--- app.js
+++ app.js
@@ -1,3 +1,108 @@
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var MongoClient = require('mongodb').MongoClient;
+//var req = require('make-runnable');
+
+//these are the only file types we will support for now
+extensions = {
+    ".html" : "text/html",
+    ".css" : "text/css",
+    ".js" : "application/javascript",
+    ".png" : "image/png",
+    ".gif" : "image/gif",
+    ".jpg" : "image/jpeg",
+    ".jpeg" : "image/jpeg"
+};
+
+
+//helper function handles file verification
+function getFile(filePath,res,/*page404,*/mimeType){
+    //does the requested file exist?
+    fs.exists(filePath,function(exists){
+        //if it does...
+        if(exists){
+            //read the fiule, run the anonymous function
+            fs.readFile(filePath,function(err,contents){
+                if(!err){
+            //console.dir("filePath: " + filePath); DEBUG
+                    //if there was no error
+                    //send the contents with the default 200/ok header
+                    res.writeHead(200,{
+                        "Content-type" : mimeType,
+                        "Content-Length" : contents.length
+                    });
+                    res.end(contents);
+                } else {
+                    console.dir(err);
+                };
+            });
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        };
+    });
+};
+
+//a helper function to handle HTTP requests
+function requestHandler(req, res) {
+    var
+    fileName = path.basename(req.url) || 'home.html',
+    ext = path.extname(fileName),
+     localFolder = __dirname + '/html/';
+    // page404 = localFolder + '404.html';
+    
+    // console.log("EXT:" + ext);
+    if(ext === '.html' && fileName != 'home.html'){
+        localFolder = __dirname + '/html/single/'; //TESTANDO SEM O SPA
+    }else if(ext === '.js'){
+        localFolder = __dirname + '/script/';
+    }else if(ext === '.css'){
+        localFolder = __dirname + '/css/';
+    }else if(ext === '.jpg' || ext === '.png' || ext === '.jpeg'){
+        localFolder = __dirname + '/img/'
+    }else{
+        console.log("Lendo arquivo sem extensão");
+        fileName = 'home.html';
+        localFolder = __dirname + '/html/';
+        ext = '.html';
+    }
+    /* Debug
+    console.dir("ext: " + ext);
+    console.dir("fileName: " + fileName);
+    console.dir("localFolder: " + localFolder);
+    */
+
+    //do we support the requested file type?
+    // if(!extensions[ext]){
+    //     //for now just send a 404 and a short message
+    //     res.writeHead(404, {'Content-Type': 'text/html'});
+    //     res.end("&lt;html&gt;&lt;head&gt;&lt;/head&gt;&lt;body&gt;The requested file type is not supported&lt;/body&gt;&lt;/html&gt;");
+    // };
+
+    getFile((localFolder + fileName),res, /*page404,*/extensions[ext]);
+};
+
+//step 2) create the server
+    http.createServer(requestHandler)
+
+ //step 3) listen for an HTTP request on port 3000
+    // .listen(3000);
+    // console.log('Node server is running on http://localhost:3000');
+
+
+//Até as funções utilizadas só manipulavam a requisição de arquivo
+/////////////////////////////////////////////////////////////////////////////////////
+
+//module.exports.start = function(){
+    console.log("Entrou");
+
+
+
+
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -8,7 +113,9 @@ var url = "mongodb://localhost:27017/";
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.resolve(__dirname, 'html')));
+app.use(express.static(path.resolve(__dirname, 'html')), function(){
+    requestHandler();
+}); 
 
 
 MongoClient.connect(url, function(err, db) { //Estabelecendo a conexão no formato W3School
--- server.js
+++ server.js
@@ -1,108 +1,21 @@
-var http = require('http');
 var path = require('path');
-var fs = require('fs');
-//var req = require('make-runnable');
-
-//these are the only file types we will support for now
-extensions = {
-    ".html" : "text/html",
-    ".css" : "text/css",
-    ".js" : "application/javascript",
-    ".png" : "image/png",
-    ".gif" : "image/gif",
-    ".jpg" : "image/jpeg",
-    ".jpeg" : "image/jpeg"
-};
-
-
-//helper function handles file verification
-function getFile(filePath,res,/*page404,*/mimeType){
-    //does the requested file exist?
-    fs.exists(filePath,function(exists){
-        //if it does...
-        if(exists){
-            //read the fiule, run the anonymous function
-            fs.readFile(filePath,function(err,contents){
-                if(!err){
-            //console.dir("filePath: " + filePath); DEBUG
-                    //if there was no error
-                    //send the contents with the default 200/ok header
-                    res.writeHead(200,{
-                        "Content-type" : mimeType,
-                        "Content-Length" : contents.length
-                    });
-                    res.end(contents);
-                } else {
-                    console.dir(err);
-                };
-            });
-        } else {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        };
-    });
-};
-
-//a helper function to handle HTTP requests
-function requestHandler(req, res) {
-    var
-    fileName = path.basename(req.url) || 'home.html',
-    ext = path.extname(fileName),
-     localFolder = __dirname + '/html/';
-    // page404 = localFolder + '404.html';
-    
-    if(ext === '.html' /*&& fileName != 'home.html' */){
-        localFolder = __dirname + '/html/'; //TESTANDO SEM O SPA
-    }else if(ext === '.js'){
-        localFolder = __dirname + '/script/';
-    }else if(ext === '.css'){
-        localFolder = __dirname + '/css/';
-    }else if(ext === '.jpg' || ext === '.png' || ext === '.jpeg')
-        localFolder = __dirname + '/img/';
-
-    /* Debug
-    console.dir("ext: " + ext);
-    console.dir("fileName: " + fileName);
-    console.dir("localFolder: " + localFolder);
-    */
-
-    //do we support the requested file type?
-    if(!extensions[ext]){
-        //for now just send a 404 and a short message
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end("&lt;html&gt;&lt;head&gt;&lt;/head&gt;&lt;body&gt;The requested file type is not supported&lt;/body&gt;&lt;/html&gt;");
-    };
-
-    getFile((localFolder + fileName),res, /*page404,*/extensions[ext]);
-};
-
-//step 2) create the server
-    http.createServer(requestHandler)
-
- //step 3) listen for an HTTP request on port 3000
-    .listen(8080);
-
-
-    console.log('Node server is running on http://localhost:8080');
-//Até as funções utilizadas só manipulavam a requisição de arquivo
-/////////////////////////////////////////////////////////////////////////////////////
-
-//module.exports.start = function(){
-    console.log("Entrou");
-
-
 var express = require('express');
-var path = require('path');
 var bodyParser = require('body-parser');
 
 var MongoClient = require('mongodb').MongoClient;
+
 var url = "mongodb://localhost:27017/";
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, 'html')));
+app.use('/css', express.static(path.resolve(__dirname, 'css')));
+app.use('/script',express.static(path.resolve(__dirname, 'script')));
+app.use('/img', express.static(path.resolve(__dirname, 'img')));
 
+
+    console.log("Entrou");
 
 MongoClient.connect(url, function(err, db) { //Estabelecendo a conexão no formato W3School
   if (err) throw err;
@@ -121,7 +34,8 @@ MongoClient.connect(url, function(err, db) { //Estabelecendo a conexão no forma
 
 });
 
-
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0');
+console.log('Node server is running on http://localhost:3000');
 
 //require('make-runnable');
 
