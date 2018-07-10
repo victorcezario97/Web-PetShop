462111b67514180d28c9d50b854b2e55776d20ed
victorcezario97 <victorscezario@hotmail.com>
2018-07-09 22:15:09 -0300

    Fixed require error and removed debug print

--- script/main.js
+++ script/main.js
@@ -27,8 +27,6 @@ function singlePage(page, anchor, schedule){
 	var xhttp = new XMLHttpRequest();
 	xhttp.open("GET", page, true);
 	xhttp.onreadystatechange = function() {
-console.log('anchor ' + anchor);
-console.log('script ' + schedule);
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
 			document.getElementById("troca").innerHTML = xhttp.responseText;
       if(anchor != null)
